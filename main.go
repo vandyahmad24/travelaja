f2b331295ebb3999cc8dad4012ec04fd32532d8a
Vandy Ahmad <56667915+vandyahmad24@users.noreply.github.com>
2022-07-28 13:16:49 +0700

    Update main.go

--- main.go
+++ main.go
@@ -114,8 +114,6 @@ func CountDifferenctArray(str1, str2 string) {
 func ArrayIntersect(slice1 []string, slice2 []string) bool {
 	var diff []string
 
-	// Loop two times, first to find slice1 strings not in slice2,
-	// second loop to find slice2 strings not in slice1
 	for i := 0; i < 2; i++ {
 		for _, s1 := range slice1 {
 			found := false
@@ -125,12 +123,10 @@ func ArrayIntersect(slice1 []string, slice2 []string) bool {
 					break
 				}
 			}
-			// String not found. We add it to return slice
 			if !found {
 				diff = append(diff, s1)
 			}
 		}
-		// Swap the slices, only if it was the first loop
 		if i == 0 {
 			slice1, slice2 = slice2, slice1
 		}
@@ -140,6 +136,4 @@ func ArrayIntersect(slice1 []string, slice2 []string) bool {
 		return false
 	}
 	return true
-
-	// return diff
 }
